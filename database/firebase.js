a0db4cd187193381518520f7dc6ad773c42debd4
esrabayramova <59417568+esrabayramova@users.noreply.github.com>
2021-06-15 21:28:49 +0400

    Update firebase.js

--- database/firebase.js
+++ database/firebase.js
@@ -1,15 +1,19 @@
 import firebase from 'firebase';
 import 'firebase/firestore'
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration (source code from your firebase project)
+
+/*
 var firebaseConfig = {
-   apiKey: "AIzaSyBavpgQ9s3f8NeBEcXkVLVpxrB15_XIIcU",
-   authDomain: "reactnativefirebase-e2fba.firebaseapp.com",
-   projectId: "reactnativefirebase-e2fba",
-   storageBucket: "reactnativefirebase-e2fba.appspot.com",
-   messagingSenderId: "22092573756",
-   appId: "1:22092573756:web:81724d2d6a6692b0f26739"
+   apiKey: "---",
+   authDomain: "---",
+   projectId: "---",
+   storageBucket: "---",
+   messagingSenderId: "---",
+   appId: "---"
 };
+*/
+
  // Initialize Firebase
  firebase.initializeApp(firebaseConfig);
 
